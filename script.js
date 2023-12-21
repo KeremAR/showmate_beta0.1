086174920f98cdbfa246c929dc4ebce6bf0ff3d1
Kerem AR <keremar0000@gmail.com>
2023-12-21 23:18:43 +0300

    json updated
    seasons in json updated

--- script.js
+++ script.js
@@ -1,55 +1,73 @@
+// Fetch data from TVData.json
 fetch("TVData.json")
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.statusText}`);
+        }
+        return response.json();
+    })
     .then(data => {
-        const watchingShows = [];
-        const notStartedShows = [];
-        const endedShows = [];
-
-        data.forEach(show => {
-            const hasWatchedEpisodes = show.episodes.some(episode => episode.watched);
-            const allEpisodesWatched = show.episodes.every(episode => episode.watched);
-
-            if (hasWatchedEpisodes) {
-                watchingShows.push(show);
-            } else if (allEpisodesWatched) {
-                endedShows.push(show);
-            } else {
-                notStartedShows.push(show);
-            }
-        });
-
-        displayShows("watchingShows", watchingShows);
-        displayShows("notStartedShows", notStartedShows);
-        displayShows("endedShows", endedShows);
-    });
+        console.log("Fetched Data:", data);
+        displayShowsByStatus(data); // Pass the fetched data to the display function
+    })
+    .catch(error => console.error("Fetch Error:", error));
 
 function displayShows(containerId, shows) {
     const container = document.getElementById(containerId);
+    container.innerHTML = ""; // Clear previous content
 
     shows.forEach(show => {
         const showDiv = document.createElement("div");
         showDiv.classList.add("tv-show");
         showDiv.innerHTML = `
-            <h3>${show.title}</h3>
-            <p>Genre: ${show.genre}</p>
-            <p>Network: ${show.network}</p>
-            <button onclick="openShowPage(${JSON.stringify(show)})">View Details</button>
-        `;
+                <h3>${show.title}</h3>
+                <p>Genre: ${show.genre}</p>
+                <p>Network: ${show.network}</p>
+                <a href="showpage.html?show=${encodeURIComponent(JSON.stringify(show))}" target="_blank">View Details</a>
+            `;
         container.appendChild(showDiv);
     });
 }
 
-function openShowPage(tvShow) {
-    // Implement logic to open an individual show page with detailed information
-    // Pass the relevant show data to the individual show page
-    // You can use a modal, a new page, or any other UI element for this purpose
-    displayIndividualShowPage(tvShow);
-}
 
-function displayIndividualShowPage(tvShow) {
-    // Implement logic to display detailed information about the show, including seasons and episodes
-    // Allow users to toggle the watched property of episodes
-    // Update the JSON file or store the data persistently in another way to reflect the changes
-    // You can use a modal, a new page, or any other UI element for this purpose
-    alert(`Details for ${tvShow.title}:\n${JSON.stringify(tvShow, null, 2)}`);
+
+function displayShowsByStatus(data) {
+    console.log("Displaying shows by status");
+
+    const watchingShows = [];
+    const notStartedShows = [];
+    const endedShows = [];
+
+    data.forEach(show => {
+        let isWatching = false;
+        let isEnded = true;
+
+        show.seasons.forEach(season => {
+            for (const [seasonKey, episodes] of Object.entries(season)) {
+                episodes.forEach(episode => {
+                    if (episode.watched) {
+                        isWatching = true;
+                    } else {
+                        isEnded = false;
+                    }
+                });
+            }
+        });
+
+        if (isWatching) {
+            watchingShows.push(show);
+        } else if (isEnded) {
+            endedShows.push(show);
+        } else {
+            notStartedShows.push(show);
+        }
+    });
+
+    console.log("Watching Shows:", watchingShows);
+    console.log("Not Started Shows:", notStartedShows);
+    console.log("Ended Shows:", endedShows);
+
+    displayShows("watchingShows", watchingShows);
+    displayShows("notStartedShows", notStartedShows);
+    displayShows("endedShows", endedShows);
 }
