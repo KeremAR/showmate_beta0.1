0f8e4d8d8fcc9f9297008e722feb4c5e35630232
Kerem AR <keremar0000@gmail.com>
2023-12-26 21:04:51 +0300

    episode buttons
    episode buttons working on showpage

--- showpage.js
+++ showpage.js
@@ -25,7 +25,27 @@ function displaySeasons(seasons) {
         seasonDiv.classList.add("season");
         seasonDiv.innerHTML = `
             <h3>${season.season}</h3>
-            <div class="episodes-container" id="season-${seasonIndex}"></div>
+            <div class="episodes-container" id="season-${seasonIndex}">
+            <button onclick="toggleWatched('season', ${season.id})">Mark ${season.id in localStorage ? 'Unwatched' : 'Watched'}</button>
+            </div>
+        `;
+        seasonsContainer.appendChild(seasonDiv);
+
+        displayEpisodes(`season-${seasonIndex}`, season.episodes);
+    });
+}
+
+function displaySeasons(seasons) {
+    const seasonsContainer = document.getElementById("seasons");
+
+    seasons.forEach((season, seasonIndex) => {
+        const seasonDiv = document.createElement("div");
+        seasonDiv.classList.add("season");
+        seasonDiv.innerHTML = `
+            <h3>${season.season}</h3>
+            <div class="episodes-container" id="season-${seasonIndex}">
+            <button onclick="toggleWatched('season', ${season.id})">Mark ${season.id in localStorage ? 'Unwatched' : 'Watched'}</button>
+            </div>
         `;
         seasonsContainer.appendChild(seasonDiv);
 
@@ -40,16 +60,43 @@ function displayEpisodes(seasonId, episodes) {
         const episodeDiv = document.createElement("div");
         episodeDiv.classList.add("episode");
 
+        const key = `episode_${episode.id}`;
+        const isWatched = localStorage.getItem(key);
+
         episodeDiv.innerHTML = `
             <div class="episode-details">
                 <h3>${episode.episodeNumber}</h3>
                 <h4>${episode.title}</h4>
                 <p>${episode.description}</p>
                 <p>Air Date: ${episode.airDate}</p>
-                <p>Watched: ${episode.id in localStorage ? 'Yes' : 'No'}</p>
+                <p>Watched: ${isWatched ? 'Yes' : 'No'}</p>
+                <button onclick="toggleWatched('episode', ${episode.id}, '${seasonId}')">Mark ${isWatched ? 'Unwatched' : 'Watched'}</button>
                 <a href="episodepage.html?episode=${encodeURIComponent(JSON.stringify(episode))}" target="_blank">View Episode</a>
             </div>
         `;
         episodesContainer.appendChild(episodeDiv);
     });
+}
+
+
+function toggleWatched(type, id, seasonId) {
+    const key = `${type}_${id}`;
+    console.log(`Toggling ${key}. Was Watched: ${localStorage.getItem(key) === 'watched'}`);
+
+    if (localStorage.getItem(key) === 'watched') {
+        localStorage.removeItem(key);
+    } else {
+        localStorage.setItem(key, 'watched');
+    }
+
+    // Update the button text using the key
+    updateButtonText(type, id, seasonId, key);
+}
+
+function updateButtonText(type, id, seasonId, key) {
+    // Update the button text
+    const button = document.querySelector(`[onclick="toggleWatched('${type}', ${id}, '${seasonId}')"]`);
+    if (button) {
+        button.textContent = `Mark ${localStorage.getItem(key) ? 'Unwatched' : 'Watched'}`;
+    }
 }
